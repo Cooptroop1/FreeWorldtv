d48e9657e9bf789ebf7ae323db764e706ae20702
Cooptroop1 <jcooper4888@aol.co.uk>
2026-02-24 18:36:33 +0000

    Add service worker for caching and fetch handling

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+// public/sw.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('freestream-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/icon-192.png',
+        '/icon-512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
