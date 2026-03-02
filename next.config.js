fde4321b4fd0c06076bce6115d716dabc5ea3e93
Cooptroop1 <jcooper4888@aol.co.uk>
2026-03-02 12:12:27 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,15 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'image.tmdb.org',
+        hostname: 'image.tmdb.org',        // already had this for posters
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.watchmode.com',     // ← THIS IS THE MISSING LINE
       },
     ],
   },
+  // (keep any other settings you already have)
 };
 
 module.exports = nextConfig;
