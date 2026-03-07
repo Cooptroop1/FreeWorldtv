82654f950b5e65c4711ff9fd45397447a5d8821c
Cooptroop1 <jcooper4888@aol.co.uk>
2026-03-07 21:30:14 +0000

    Update page.tsx

--- app/my-links/page.tsx
+++ app/my-links/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
-import MainApp from '../../_components/MainApp';
-
+import MainApp from '../_components/MainApp';
 export const metadata: Metadata = {
   title: 'My Links | FreeStream World',
   description: 'Your saved direct links to free streaming content.',
