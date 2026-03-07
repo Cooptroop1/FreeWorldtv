7b8bab16bc9835c579b09ca12212c20ce506d075
Cooptroop1 <jcooper4888@aol.co.uk>
2026-03-07 22:02:19 +0000

    Update page.tsx

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import MainApp from '../_components/MainApp';
+export const dynamic = 'force-static';
 
 export const metadata: Metadata = {
   title: 'Discover - Free Movies & TV Shows | FreeStream World',
