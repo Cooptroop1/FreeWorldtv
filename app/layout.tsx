c2b49c1ce7707524f0d7ff4e445d821a5386e8ea
Cooptroop1 <jcooper4888@aol.co.uk>
2026-03-10 16:23:18 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
             <head>
         {/* Google Site Verification */}
         <meta name="google-site-verification" content="v5peivsBuVQvw32Su3UT4btwIbndtLT1Eg4JGPDhM_E" />
